39ff6614aa9e2cf975c38830a6b48bb79da29741
tonymushah <tonyramanantsalama@gmail.com>
2025-04-24 07:37:58 +0300

    MessageBoxA

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::ffi::CString;
+
+use windows::Win32::{
+    Foundation::HWND,
+    UI::WindowsAndMessaging::{MB_OK, MessageBoxA, MessageBoxW},
+};
+use windows_core::{HSTRING, PCSTR, PCWSTR};
+
 fn main() {
-    println!("Hello, world!");
+    let lptext = "sdffdsfdsTony";
+
+    let lpcaption = "AAAaaaaaaaaaaaaaadsads sadsad a";
+
+    unsafe {
+        {
+            let lptext = CString::new(lptext).unwrap();
+            let lpcaption = CString::new(lpcaption).unwrap();
+            let res = MessageBoxA(
+                None,
+                PCSTR::from_raw(lptext.as_ptr() as *const u8),
+                PCSTR::from_raw(lpcaption.as_ptr() as *const u8),
+                MB_OK,
+            );
+            println!("{:?}", res);
+        }
+
+    };
 }
