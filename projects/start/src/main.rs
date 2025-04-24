45470d8fa5d3cf3ddbdfebe85dbec5a2064d0d29
tonymushah <tonyramanantsalama@gmail.com>
2025-04-24 19:51:06 +0300

    get process id

--- projects/start/src/main.rs
+++ projects/start/src/main.rs
@@ -1,24 +1,38 @@
-use windows::{Win32::System::Threading::*, core::*};
+use std::ffi::{CStr, CString};
 
-static COUNTER: std::sync::RwLock<i32> = std::sync::RwLock::new(0);
+use windows::Win32::System::Diagnostics::ToolHelp::*;
+use windows_core::Owned;
 
-extern "system" fn callback(_: PTP_CALLBACK_INSTANCE, _: *mut std::ffi::c_void, _: PTP_WORK) {
-    let mut counter = COUNTER.write().unwrap();
-    *counter += 1;
+#[derive(Debug, thiserror::Error)]
+#[error(transparent)]
+enum GetProcessIdError {
+    Null(#[from] std::ffi::NulError),
+    Windows(#[from] windows_core::Error),
+}
+
+fn get_process_id<T>(target: &T) -> Result<u32, GetProcessIdError>
+where
+    T: AsRef<str> + ?Sized,
+{
+    let mut entry = PROCESSENTRY32 {
+        dwSize: size_of::<PROCESSENTRY32>() as u32,
+        ..Default::default()
+    };
+    let target_c_string = CString::new(target.as_ref())?;
+    unsafe {
+        let handle = Owned::new(CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0)?);
+        Process32First(*handle, &mut entry)?;
+        loop {
+            Process32Next(*handle, &mut entry)?;
+            let process_exe = CStr::from_ptr(entry.szExeFile.as_ptr());
+            if process_exe == target_c_string.as_c_str() {
+                return Ok(entry.th32ProcessID);
+            }
+        }
+    }
 }
 
 fn main() -> anyhow::Result<()> {
-    unsafe {
-        let work = CreateThreadpoolWork(Some(callback), None, None)?;
-        for _ in 0..10 {
-            SubmitThreadpoolWork(work);
-        }
-        WaitForThreadpoolWorkCallbacks(work, false);
-        CloseThreadpoolWork(work);
-    };
-    {
-        let counter = COUNTER.read().unwrap();
-        println!("counter: {}", *counter);
-    }
+    println!("notepad.exe [{}]", get_process_id("notepad.exe")?);
     Ok(())
 }
