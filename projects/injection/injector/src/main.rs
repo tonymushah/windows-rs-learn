173746365b64ac8b5b3401c3903172b9c06a7535
tonymushah <tonyramanantsalama@gmail.com>
2025-04-24 23:17:29 +0300

    added main

--- projects/injection/injector/src/main.rs
+++ projects/injection/injector/src/main.rs
@@ -1,3 +1,21 @@
+use injector::injection::Process;
+
+fn run() -> anyhow::Result<()> {
+    let mut env_args = std::env::args();
+    let target = env_args
+        .next()
+        .ok_or(anyhow::anyhow!("please input exe target"))?;
+    let dll_path = env_args
+        .next()
+        .ok_or(anyhow::anyhow!("cannot find dll path"))?;
+    let process = Process::open_by_exe_name(&target)?;
+    process.inject_dll(&dll_path)?;
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = run() {
+        eprintln!("{err}");
+        std::process::exit(1);
+    }
 }
