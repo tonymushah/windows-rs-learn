b4385e9e3f1db1ea161cc3b6ab6109d9017466d2
tonymushah <tonyramanantsalama@gmail.com>
2025-04-26 00:03:41 +0300

    added `main_work` module

--- projects/injection/dll-to-inject/src/lib.rs
+++ projects/injection/dll-to-inject/src/lib.rs
@@ -1,27 +1,29 @@
+mod main_work;
 mod run;
 pub mod utils;
 
-use std::{os::raw::c_void, panic, sync::RwLock};
+use std::{os::raw::c_void, panic};
 
+use main_work::{init_work, wait_for_work};
 use utils::message_box;
 use windows::Win32::{
     Foundation::HMODULE,
-    System::{
-        SystemServices::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH},
-        Threading::{
-            /*CreateThread,*/ CreateThreadpoolWork, PTP_CALLBACK_INSTANCE, PTP_WORK,
-            SubmitThreadpoolWork, WaitForThreadpoolWorkCallbacks,
-        },
-    },
+    System::SystemServices::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH},
 };
 use windows_core::BOOL;
 
-static WORK: RwLock<Option<PTP_WORK>> = RwLock::new(None);
-
-extern "system" fn run_work(_pfnwk: PTP_CALLBACK_INSTANCE, _ctx: *mut c_void, _work: PTP_WORK) {
-    if let Err(err) = run::run(HMODULE(_ctx)) {
-        message_box("Cannot execute payload", &err.to_string());
-    }
+fn set_panic_hook() {
+    panic::set_hook(Box::new(|d| {
+        message_box("Panic!", {
+            if let Some(d) = d.payload().downcast_ref::<String>() {
+                d.as_str()
+            } else if let Some(d) = d.payload().downcast_ref::<&'static str>() {
+                d
+            } else {
+                "..."
+            }
+        });
+    }));
 }
 
 #[allow(non_snake_case, clippy::missing_safety_doc)]
@@ -33,40 +35,13 @@ pub unsafe extern "system" fn DllMain(
 ) -> BOOL {
     match ul_reason_for_call {
         DLL_PROCESS_ATTACH => {
-            panic::set_hook(Box::new(|d| {
-                message_box("Panic!", {
-                    if let Some(d) = d.payload().downcast_ref::<String>() {
-                        d.as_str()
-                    } else if let Some(d) = d.payload().downcast_ref::<&'static str>() {
-                        d
-                    } else {
-                        "..."
-                    }
-                });
-            }));
+            set_panic_hook();
             message_box("Attached", "Your dll has been injected");
-            WORK.clear_poison();
-            let mut write_work = WORK.write().unwrap();
-            // CreateThread(lpthreadattributes, dwstacksize, lpstartaddress, lpparameter, dwcreationflags, lpthreadid)
-            if write_work.is_none() {
-                match unsafe { CreateThreadpoolWork(Some(run_work), Some(_hmodule.0), None) } {
-                    Ok(work) => {
-                        unsafe { SubmitThreadpoolWork(work) };
-                        write_work.replace(work);
-                    }
-                    Err(err) => {
-                        message_box("Cannot create thread work", &err.message());
-                    }
-                }
-            }
+            init_work(_hmodule);
         }
         DLL_PROCESS_DETACH => {
+            wait_for_work(true);
             message_box("Dettached", "Dettached dll");
-            WORK.clear_poison();
-            let mut write_work = WORK.write().unwrap();
-            if let Some(work) = write_work.take() {
-                unsafe { WaitForThreadpoolWorkCallbacks(work, true) };
-            }
         }
         _ => {}
     }
---
+++ projects/injection/dll-to-inject/src/main_work.rs
@@ -0,0 +1,45 @@
+use crate::utils::message_box;
+use windows::Win32::{
+    Foundation::HMODULE,
+    System::Threading::{
+        /*CreateThread,*/ CreateThreadpoolWork, PTP_CALLBACK_INSTANCE, PTP_WORK,
+        SubmitThreadpoolWork, WaitForThreadpoolWorkCallbacks,
+    },
+};
+
+use std::{os::raw::c_void, sync::RwLock};
+
+static WORK: RwLock<Option<PTP_WORK>> = RwLock::new(None);
+
+pub fn wait_for_work(cancel: bool) {
+    WORK.clear_poison();
+    let mut write_work = WORK.write().unwrap();
+    if let Some(work) = write_work.take() {
+        unsafe { WaitForThreadpoolWorkCallbacks(work, cancel) };
+    }
+}
+
+extern "system" fn run_work(_pfnwk: PTP_CALLBACK_INSTANCE, _ctx: *mut c_void, _work: PTP_WORK) {
+    if let Err(err) = crate::run::run(HMODULE(_ctx)) {
+        message_box("Cannot execute payload", &err.to_string());
+    }
+    WORK.clear_poison();
+    WORK.write().unwrap().take();
+}
+
+pub fn init_work(_hmodule: HMODULE) {
+    WORK.clear_poison();
+    let mut write_work = WORK.write().unwrap();
+    // CreateThread(lpthreadattributes, dwstacksize, lpstartaddress, lpparameter, dwcreationflags, lpthreadid)
+    if write_work.is_none() {
+        match unsafe { CreateThreadpoolWork(Some(run_work), Some(_hmodule.0), None) } {
+            Ok(work) => {
+                unsafe { SubmitThreadpoolWork(work) };
+                write_work.replace(work);
+            }
+            Err(err) => {
+                message_box("Cannot create thread work", &err.message());
+            }
+        }
+    }
+}
