fe34035d370547801d2af903f91a51700590141d
tonymushah <tonyramanantsalama@gmail.com>
2025-04-26 13:50:01 +0300

    Debug only level

--- projects/injection/dll-to-inject/src/logger.rs
+++ projects/injection/dll-to-inject/src/logger.rs
@@ -2,7 +2,8 @@ use fern::{Dispatch, InitError};
 
 pub fn setup_log() -> Result<(), InitError> {
     Dispatch::new()
-        .chain(crate::run::dis_log::output())
+        .level(log::LevelFilter::Debug)
+        .chain(Dispatch::new().chain(crate::run::dis_log::output()))
         .apply()?;
     Ok(())
 }
