b5576d5901cd31f1f441e80e96316fec1eb523a7
tonymushah <tonyramanantsalama@gmail.com>
2025-04-27 11:24:10 +0300

    use lazy lock

--- projects/injection/dll-to-inject/src/run/loaded_modules.rs
+++ projects/injection/dll-to-inject/src/run/loaded_modules.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Debug, ptr::NonNull};
+use std::{fmt::Debug, os::raw::c_void, sync::LazyLock};
 
 use eframe::egui::{Widget, mutex::RwLock};
 use windows::Win32::System::Threading::{
@@ -11,46 +11,56 @@ use crate::utils::get_loaded_module::{ModuleEntry, modules};
 
 pub struct LoadedModules {
     work: Owned<PTP_WORK>,
-    modules: RwLock<Vec<ModuleEntry>>,
 }
 
 impl Debug for LoadedModules {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("LoadedModules")
             .field("work", &self.work)
-            .field("modules", &"RwLock<Vec<ModuleEntry>>")
             .finish()
     }
 }
 
+static MODULES: LazyLock<RwLock<Vec<ModuleEntry>>> =
+    LazyLock::new(|| RwLock::<Vec<ModuleEntry>>::new(Vec::new()));
+
 extern "system" fn load_module_work(
     _instance: PTP_CALLBACK_INSTANCE,
-    context: *mut core::ffi::c_void,
+    _context: *mut core::ffi::c_void,
     _work: PTP_WORK,
 ) {
-    let module_rw = context.cast::<RwLock<Vec<ModuleEntry>>>();
-    if let Some(module_rw) = NonNull::new(module_rw) {
-        if let Ok(mods) = modules(Some(unsafe { GetCurrentProcessId() })) {
-            unsafe {
-                let rw = module_rw.read();
-                *rw.write() = mods;
-            }
+    if _context == (&raw const MODULES as *mut c_void) {
+        log::info!("Same!");
+    }
+    match modules(Some(unsafe { GetCurrentProcessId() })) {
+        Ok(mods) => {
+            *MODULES.write() = mods;
+        }
+        Err(err) => {
+            log::error!("{err}");
         }
     }
 }
 
 impl Default for LoadedModules {
     fn default() -> Self {
-        let mut modules = RwLock::new(Vec::new());
         let work = {
             unsafe {
-                CreateThreadpoolWork(Some(load_module_work), Some(&raw mut modules as _), None)
-                    .unwrap_or_default()
+                match CreateThreadpoolWork(
+                    Some(load_module_work),
+                    Some(&raw const MODULES as _),
+                    None,
+                ) {
+                    Ok(w) => w,
+                    Err(err) => {
+                        log::error!("{err}");
+                        Default::default()
+                    }
+                }
             }
         };
         Self {
             work: unsafe { Owned::new(work) },
-            modules,
         }
     }
 }
@@ -61,7 +71,7 @@ impl Widget for &mut LoadedModules {
             SubmitThreadpoolWork(*self.work);
         }
         ui.scope(|ui| {
-            for entry in self.modules.read().iter() {
+            for entry in MODULES.read().iter() {
                 ui.label(format!("{} [{}]", entry.name, entry.path));
             }
         })
