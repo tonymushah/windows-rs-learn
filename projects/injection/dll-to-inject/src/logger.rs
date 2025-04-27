8f6287ab40656840535f4313ad353a0ed0b65bed
tonymushah <tonyramanantsalama@gmail.com>
2025-04-27 11:23:14 +0300

    eframe dbug max

--- projects/injection/dll-to-inject/src/logger.rs
+++ projects/injection/dll-to-inject/src/logger.rs
@@ -3,6 +3,7 @@ use fern::{Dispatch, InitError};
 pub fn setup_log() -> Result<(), InitError> {
     Dispatch::new()
         .level(log::LevelFilter::Debug)
+        .level_for("eframe", log::LevelFilter::Debug)
         .chain(Dispatch::new().chain(crate::run::dis_log::output()))
         .apply()?;
     Ok(())
